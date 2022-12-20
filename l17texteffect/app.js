dd7c8979d3ad93df017c706648c0a09a9c5246f3
hein khant <heinkhantzaw14720@gmail.com>
2022-12-20 18:08:05 +0630

    Day2

---
+++ l17texteffect/app.js
@@ -0,0 +1,12 @@
+//Get Ui
+const language = ['Nodejs','Reactjs','Vuejs','Laravel'];
+const colors = ['red','skyblue','violet','yellow'];
+const gettxtani = document.querySelector('.txtani');
+// console.log(gettxtani);
+
+// console.log(language[0]);                            //Nodejs
+// console.log(language[1]);
+// console.log(language[2]);
+
+// console.log(language.indexOf('Nodejs'));             // 0
+// console.log(colors[language.indexOf('Nodejs')]);     //red
